c600c61ef99875ab10a6ac2b9c14289f29f82ff5
Yaakov Block <jacobblockhere@gmail.com>
2024-04-01 01:45:32 +0300

    set up editor

--- website/app.py
+++ website/app.py
@@ -1,22 +1,19 @@
 #libraries
 from flask import Flask, render_template
-import rocher
-
-monaco_html = rocher.editor_html(
-    "static/vs",
-    "editor",
-    "python",
-    "print('Hello World!')",
-)
+import rocher.flask
 
 # simulator
 from .yuda_asm_simulator import *
 
 app = Flask(__name__)
 
+# Register the editor with the Flask app
+# and expose the rocher_editor function to Jinja templates
+rocher.flask.editor_register(app)
+
 @app.route("/")
 def main():
-    return render_template("main_template.html", monaco_html = monaco_html)
+    return render_template("main_template.html", source_code="print('test')")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
