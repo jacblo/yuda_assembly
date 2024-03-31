72a52a7826abe603b4d4d0ee56c42ecc3b060086
Yaakov Block <jacobblockhere@gmail.com>
2024-03-31 23:46:02 +0300

    starting working on website

---
+++ website/app.py
@@ -0,0 +1,22 @@
+#libraries
+from flask import Flask, render_template
+import rocher
+
+monaco_html = rocher.editor_html(
+    "",
+    "editor",
+    "python",
+    "print('Hello World!')",
+)
+
+# simulator
+from .yuda_asm_simulator import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("main_template.html", monaco_html = monaco_html)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
